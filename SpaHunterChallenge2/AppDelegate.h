7811c39187ffc307d74fd8beeac77d3573bbfa44
Christina Gunarto <cgunarto@gmail.com>
2014-11-07 08:39:59 -0800

    Initial Commit

---
+++ SpaHunterChallenge2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpaHunterChallenge2
+//
+//  Created by CHRISTINA GUNARTO on 11/7/14.
+//  Copyright (c) 2014 Christina Gunarto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
